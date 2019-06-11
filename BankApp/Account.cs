43c8aa7488007e1a7e745bb88728ec63b2f19326
Rauni <rakangas@microsoft.com>
2019-06-11 15:41:40 -0700

    Updated Program.cs with basic menu, and modified some of the Account.cs properties

--- BankApp/Account.cs
+++ BankApp/Account.cs
@@ -4,20 +4,39 @@ using System.Text;
 
 namespace BankApp
 {
+    enum AccountTypes
+    {
+        Checking,
+        Savings,
+        CD,
+        Loan
+    }
+
+
     /// <summary>
     /// Class that defines a bank account
     /// </summary>
     class Account
     {
+        private static int lastAccountNumber = 0;
+
+        #region Constructors
+        public Account()
+        {
+            AccountNumber = ++lastAccountNumber;
+            CreateDate = DateTime.Now;
+        }
+        #endregion
+
         #region Properties
         /// <summary>
         /// Get/Set Account Number property
         /// </summary>
         public int AccountNumber { get; private set; }
-        public decimal AccountBalance { get; set; }
+        public decimal AccountBalance { get; private set; }
         public string EmailAddress { get; set; }
-        public string AccountType { get; set; }
-        public DateTime CreateDate { get; set; }
+        public AccountTypes AccountType { get; set; }
+        public DateTime CreateDate { get; private set; }
         public string AccountName { get; set; }
         #endregion
 
--- BankApp/Program.cs
+++ BankApp/Program.cs
@@ -6,10 +6,47 @@ namespace BankApp
     {
         static void Main(string[] args)
         {
-            var myAccount = new Account();
-            myAccount.AccountNumber = 12345;
-            myAccount.AccountName = "My Checking";
-            myAccount.AccountBalance = 10000000M;
+
+            //Console.WriteLine($"Account Number: {myAccount.AccountNumber} |" +
+            //    $" Account Type: {myAccount.AccountType} |"+
+            //    $" Account Name: {myAccount.AccountName} |" +
+            //    $" Email: {myAccount.EmailAddress} |" +
+            //    $" Balance: {myAccount.AccountBalance:C}");
+
+            Console.WriteLine("Welcome to the Banking Application");
+            while (true)
+            {
+                Console.WriteLine("0 - Exit");
+                Console.WriteLine("1 - Create New Account");
+                Console.WriteLine("2 - Deposit");
+                Console.WriteLine("3 - Withdraw");
+                Console.WriteLine("4 - Print All Accounts");
+
+                Console.Write("Select Option: ");
+                var selection = Console.ReadLine();
+
+                switch (selection)
+                {
+                    case "0":
+                        Console.WriteLine("Thank you for using our banking application!");
+                        return;
+                    case "1":
+                        Console.Write("Email Address: ");
+                        var email = Console.ReadLine();
+                        Console.Write("Account Name: ");
+                        var accountName = Console.ReadLine();
+                        break;
+                    case "2":
+                        break;
+                    case "3":
+                        break;
+                    case "4":
+                        break;
+                    default:
+                        break;
+                }
+            }
+            
 
         }
     }
