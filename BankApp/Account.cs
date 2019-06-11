38dba15e0407561afce53d86299f247f21d605f1
Rauni Kangas <rakangas@microsoft.com>
2019-06-11 14:38:51 -0700

    Add project files.

---
+++ BankApp/Account.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankApp
+{
+    /// <summary>
+    /// Class that defines a bank account
+    /// </summary>
+    class Account
+    {
+        #region Properties
+        /// <summary>
+        /// Get/Set Account Number property
+        /// </summary>
+        public int AccountNumber { get; private set; }
+        public decimal AccountBalance { get; set; }
+        public string EmailAddress { get; set; }
+        public string AccountType { get; set; }
+        public DateTime CreateDate { get; set; }
+        public string AccountName { get; set; }
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Deposit money into account
+        /// </summary>
+        /// <param name="amount">Amount to be deposited</param>
+        public void Deposit(decimal amount)
+        {
+            AccountBalance += amount;
+        }
+
+        public decimal Withdraw(decimal amount)
+        {
+            AccountBalance -= amount;
+
+            return AccountBalance;
+        }
+
+        #endregion
+    }
+}
---
+++ BankApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BankApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myAccount = new Account();
+            myAccount.AccountNumber = 12345;
+            myAccount.AccountName = "My Checking";
+            myAccount.AccountBalance = 10000000M;
+
+        }
+    }
+}
